cc4ec1b7a18f7f90e7532730a2cfb6bbddc5838f
Alfredo Ochoa <alfredo.ochoa@walmart.com>
2020-02-28 15:17:28 -0600

    create library initial Data

---
+++ src/redux/library/library.data.js
@@ -0,0 +1,59 @@
+const LibraryData = [
+  {
+    id: 1,
+    cover: "https://nexter.org/wp-content/uploads/2017/11/harry-potter-books-covers-pic.jpg",
+    author: "J. K. Rowling",
+    title: "Harry Potter and the Philosopher's Stone",
+    sinopsis: "Harry Potter and the Philosopher's Stone is a fantasy novel written by British author J. K. Rowling. The first novel in the Harry Potter series and Rowling's debut novel, it follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry."
+  },
+  {
+    id: 2,
+    cover: "https://about.canva.com/wp-content/uploads/sites/3/2015/01/children_bookcover.png",
+    author: "Anne Lowinsky",
+    title: "Fred The Lonely Monster",
+    sinopsis: "Movember Nostrilis tickler magnum pi louis xiii professor plum graeme souness sweat irrigator,, ding-dong louis xiii socially mobile louis xiii movember graeme souness magnum pi sweat irrigator, Nostrilis tickler professor plum theodore roosevelt andrew weatherall."
+  },
+  {
+    id: 3,
+    cover: "https://images-na.ssl-images-amazon.com/images/I/51huB7J1pAL._SX326_BO1,204,203,200_.jpg",
+    author: "Rice Broocks",
+    title: "The Purple Book",
+    sinopsis: "In today's uncertain world, storms will arise that test your faith in a loving heavenly Father. At times, the winds and waves of the world's deceptions will roll in and seek to erode your confidence in God. Without a firm foundation in the truth of God's Word, you may be tempted to go off course and accept less than God's best for you. However, when you hear Jesus' words, receive them, and put them into practice, you will construct a life the torrents cannot shake, 'because it was well built' (Luke 6:48)."
+  },
+  {
+    id: 4,
+    cover: "https://images-na.ssl-images-amazon.com/images/I/51uLvJlKpNL._SX321_BO1,204,203,200_.jpg",
+    author: "J. R. R. Tolkien",
+    title: "The Hobbit",
+    sinopsis: "Bilbo Baggins is a hobbit who enjoys a comfortable, unambitious life, rarely traveling any farther than his pantry or cellar. But his contentment is disturbed when the wizard Gandalf and a company of dwarves arrive on his doorstep one day to whisk him away on an adventure."
+  },
+  {
+    id: 5,
+    cover: "https://images-eu.ssl-images-amazon.com/images/I/51N0D2JrlaL.jpg",
+    author: "Cixin Liu",
+    title: "Supernova Era",
+    sinopsis: "Hugo Award winner Cixin Liu had the science fiction world spellbound with his Remembrance of Earth’s Past trilogy, but Supernova Era is something different. A standalone novel from early in the Chinese author’s career, it imagines a world where adults have gone extinct and preadolescents are forced to grow up overnight and save civilization from disappearing."
+  },
+  {
+    id: 6,
+    cover: "https://b9r8e6p9.stackpathcdn.com/wp-content/uploads/2018/06/Outsider.jpg",
+    author: "Stephen King",
+    title: "The Outsider",
+    sinopsis: "Confession: while I’ve seen many of the movies and shows on which his novels are based, I’ve never actually read a Stephen King novel (sorry, Sarah Jane!). But the menacing and equally fascinating cover of King’s imposter-thriller THE OUTSIDER drew me in right away"
+  },
+  {
+    id: 7,
+    cover: "https://images-na.ssl-images-amazon.com/images/I/41bWFwadlTL._SX329_BO1,204,203,200_.jpg",
+    author: "Peter H Diamandis",
+    title: "The Future Is Faster Than You Think",
+    sinopsis: "Then, in Bold, they chronicled the use of exponential technologies that allowed the emergence of powerful new entrepreneurs. Now the bestselling authors are back with The Future Is Faster Than You Think, a blueprint for how our world will change in response to the next ten years of rapid technological disruption."
+  },
+  {
+    id: 8,
+    cover: "https://images-eu.ssl-images-amazon.com/images/I/51HFiMevaiL.jpg",
+    author: "Arno Smets",
+    title: "Solar Energy",
+    sinopsis: "Arno Smets teaches and researches at Delft University of Technology. He presents the Massive Open Online Course (MOOC) on Solar Energy (DelftX, ET.3034 TU). Over 100,000 students have registered for the MOOC, which is available in both english and arabic."
+  },
+];
+export default LibraryData;
